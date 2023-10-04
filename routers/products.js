6815182193449c623621669fdbc93e6dded5de0a
Sonwane Chetan Bharat <sonwane.chetanbharat@hcl.com>
2023-10-04 18:20:53 +0530

    Create DELETE API for product by ID.

--- routers/products.js
+++ routers/products.js
@@ -2,6 +2,7 @@ const { Product } = require("../models/product")
 const { Category } = require("../models/category")
 const express = require("express")
 const router = express.Router()
+const mongoose = require("mongoose")
 
 ///Get All list of Products..
 router.get(`/`, async (req, res) => {
@@ -44,6 +45,60 @@ router.post(`/`, async (req, res) => {
   if (!product) return res.status(500).send("The product cannot be created")
 
   res.send(product)
+})
+
+//update API for product (PUT Method we are using here..)
+
+router.put("/:id", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).send("Invalid Product Id")
+  }
+  const category = await Category.findById(req.body.category)
+  if (!category) return res.status(400).send("Invalid Category")
+
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    {
+      new: true,
+    }
+  )
+  if (!product) return res.status(500).send("The product can't be update")
+
+  res.send(product)
+})
+
+//Delete Product API by ID
+router.delete(`/:id`, (req, res) => {
+  Product.findByIdAndRemove(req.params.id)
+    .then(product => {
+      if (product) {
+        return res.status(200).json({
+          success: true,
+          message: "The product is deleted",
+        })
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: "the product not found",
+        })
+      }
+    })
+    .catch(err => {
+      return res.status(400).json({ success: false, error: err })
+    })
 })
 
 module.exports = router
